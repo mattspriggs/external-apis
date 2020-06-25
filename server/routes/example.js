73ac76cbdf197d184f41504e41b423994f2d2da7
Lache Melvin <lache.melvin@gmail.com>
2020-06-25 14:10:51 +1200

    create example route

---
+++ server/routes/example.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const router = express.Router()
+
+// GET /api/v1/example/
+router.get('/', (req, res) => {
+  try {
+    res.send('What a great example')
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
