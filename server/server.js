6828e9b72fd68697f7493412a2d379f228be5963
Lache Melvin <lache.melvin@gmail.com>
2020-06-25 14:11:03 +1200

    setup server for routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
+
+const example = require('./routes/example')
 
 const server = express()
 
 server.use(express.json())
+server.use(cors({ origin: 'http://localhost:8080' }))
 server.use(express.static(path.join(__dirname, './public')))
+
+server.use('/api/v1/example', example)
 
 module.exports = server
