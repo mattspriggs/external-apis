f9c6bf04d2b76da1650171ab54dcefe3131de3f2
emily coco parkes <emilycoco@me.com>
2021-01-13 15:16:18 +1300

    class component ➡️ react hooks

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { getWelcome } from '../api'
 
-class App extends React.Component {
-  state = {
-    welcomeStatement: ''
-  }
-  componentDidMount () {
+function App () {
+  const [welcomeStatement, setWelcome] = useState('')
+
+  useEffect(() => {
     getWelcome()
-      .then(res => this.setState({ welcomeStatement: res.statement }))
-  }
-  render () {
-    return (
-      <h1>{this.state.welcomeStatement}</h1>
-    )
-  }
+      .then(res => {
+        setWelcome(res.statement)
+        return null
+      })
+  })
+
+  return (
+    <h1>{welcomeStatement}</h1>
+  )
 }
 
 export default App
