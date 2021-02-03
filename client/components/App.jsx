eab3da3f97be6bd2fc5ceef0c4169c45e8e4b054
Don Smith <hi@dons.dev>
2021-02-03 16:05:29 +1300

    fix eslint config and move webpack config

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,13 +3,16 @@ import React, { useState, useEffect } from 'react'
 import { getWelcome } from '../api'
 
 function App () {
-  const [welcomeStatement, setWelcome] = useState('')
+  const [welcomeStatement, setWelcomeStatement] = useState('')
 
   useEffect(() => {
     getWelcome()
       .then(res => {
-        setWelcome(res.statement)
+        setWelcomeStatement(res.statement)
         return null
+      })
+      .catch((err) => {
+        console.error(err.message)
       })
   })
 
