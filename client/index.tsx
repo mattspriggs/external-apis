93c189a03f87e62e5536a820841ad9fb90543b45
Gerard Paapu <gerard@gerardpaapu.com>
2022-11-07 15:38:49 +1300

    feat:remove experimental router loaders

--- client/index.tsx
+++ client/index.tsx
@@ -1,21 +1,7 @@
-import React from 'react'
+import { createRoot } from 'react-dom/client'
+
 import App from './components/App'
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom'
-import { getWelcome } from './apiClient'
 
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route index element={<App/>} loader={async ()=> await getWelcome()}/>
-  )
-)
-const app = document.getElementById('app') as HTMLInputElement
-ReactDOM.createRoot(app).render(
-    <RouterProvider router={router} />
-)
+document.addEventListener('DOMContentLoaded', () => {
+  createRoot(document.getElementById('app') as HTMLElement).render(<App />)
+})
