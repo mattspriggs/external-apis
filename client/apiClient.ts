cb854c761620b32bbaa3fc5ef0a2512f47c13ff3
Kelly Keating <keating.kel@gmail.com>
2023-02-02 11:43:49 +1300

    add welcome interface

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -2,8 +2,12 @@ import request from 'superagent'
 
 const serverURL = 'http://localhost:3000/api/v1'
 
+interface Welcome {
+  statement: string
+}
+
 // *** EXAMPLE ***
-export function getWelcome() {
+export function getWelcome(): Promise<Welcome> {
   return request.get(`${serverURL}/welcome`).then((response) => response.body)
 }
 // ***   ***   ***
